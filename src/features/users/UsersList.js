ebd67c93d415e1033a6244352688e2cf4b564b98
russelsy <russelgabrielsy@gmail.com>
2023-10-18 15:15:10 -0400

    Connected UsersList component to backend

--- src/features/users/UsersList.js
+++ src/features/users/UsersList.js
@@ -1,9 +1,48 @@
+import { useGetUsersQuery } from './usersApiSlice'
+import User from './User'
+
 const UsersList = () => {
-  return (
-    <div>
-      <h2>Users List</h2>
-    </div>
-  )
+  const {
+    data: users,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetUsersQuery()
+
+  let content
+
+  if (isLoading) content = <p>Loading...</p>
+
+  if (isError) {
+    content = <p className='errmsg'>{error?.data?.message}</p>
+  }
+
+  if (isSuccess) {
+
+    const { ids } = users
+
+    const tableContent = ids?.length
+      ? ids.map(userId => <User key={userId} userId={userId}/>)
+      : null
+
+    content = (
+      <table className='table table-users'>
+        <thead className='table__head'>
+          <tr>
+            <th scope='col' className='table__th user_username'>Username</th>
+            <th scope='col' className='table__th user_roles'>Roles</th>
+            <th scope='col' className='table__th user_edit'>Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableContent}
+        </tbody>
+      </table>
+    )
+  }
+
+  return content
 }
 
 export default UsersList
